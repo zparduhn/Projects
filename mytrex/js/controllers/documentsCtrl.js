f7fa1384306b84721ba4785566d199892336a1a9
Zach Parduhn <zacharyparduhn@gmail.com>
2016-02-27 14:31:22 -0700

    a few touch ups

--- mytrex/js/controllers/documentsCtrl.js
+++ mytrex/js/controllers/documentsCtrl.js
@@ -11,12 +11,22 @@ angular.module('mytrex').controller('documentsCtrl', function($scope, service){
     return false;
   };
 
+$scope.showPDF = function(pdf){
+  var pdfObj = new PDFObject({
+    url: pdf,
+    id: "pdfRendered",
+    pdfOpenParams: {
+      view: "FitH"
+  }
+  }).embed("pdfRenderer");
+};
 
-  (function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
-
-  /*
-  * Here is how you use it
-  */
+  //
+  // (function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+  //
+  // /*
+  // * Here is how you use it
+  // */
   // $(function(){
   //   $('.view-pdf').on('click',function(){
   //       var pdf_link = $(this).attr('href');
