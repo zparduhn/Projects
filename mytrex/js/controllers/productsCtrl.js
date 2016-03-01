48e8b8dc554cdca688220c9b8a93f3d28243ee0c
Zach Parduhn <zacharyparduhn@gmail.com>
2016-03-01 09:18:32 -0700

    creating branch

--- mytrex/js/controllers/productsCtrl.js
+++ mytrex/js/controllers/productsCtrl.js
@@ -11,15 +11,15 @@ angular.module('mytrex').controller('productsCtrl', function($scope, service, $t
       $scope.mxdLine = "show";
       $scope.mxd3gLine = "hidden";
       $scope.mxd3gDetails = "hidden";
-      $timeout(function () {
-        $scope.mxdDetails = "show animated fadeIn";
-      }, 750);
+      $scope.clickMore = "hidden";
+      $scope.mxdDetails = "show animated bounceInUp";
     }
     else{
       $scope.mxdLine = "hidden";
       $scope.mxdDetails = "hidden";
       $scope.mxd3gLine = "hidden";
       $scope.mxd3gDetails = "hidden";
+      $scope.clickMore = "hidden";
     }
   };
   $scope.showLine3g = function(){
@@ -27,15 +27,15 @@ angular.module('mytrex').controller('productsCtrl', function($scope, service, $t
       $scope.mxd3gLine = "show";
       $scope.mxdLine = "hidden";
       $scope.mxdDetails = "hidden";
-      $timeout(function () {
-        $scope.mxd3gDetails = "show animated fadeIn";
-      }, 750);
+      $scope.clickMore = "hidden";
+      $scope.mxd3gDetails = "show animated bounceInUp";
     }
     else{
       $scope.mxd3gLine = "hidden";
       $scope.mxd3gDetails = "hidden";
       $scope.mxdLine = "hidden";
       $scope.mxdDetails = "hidden";
+      $scope.clickMore = "hidden";
     }
   };
 });
